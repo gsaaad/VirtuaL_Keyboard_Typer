3e681a680926a2c228a7d5e3a726c1dddfad13fe
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-10-14 11:16:44 -0400

    added error count for user typing

--- g-typer/src/components/Main/Main.js
+++ g-typer/src/components/Main/Main.js
@@ -5,7 +5,9 @@ import randomwords from "random-words";
 const Main = () => {
   const [words, setWords] = useState([]);
   var errorRef = useRef(null);
+  const [slide, setSlide] = useState({ transform: "translate(0px)" });
   const [errorCount, setErrorCount] = useState(0);
+  const [distance, setDistance] = useState(-30);
   const generateWords = () => {
     for (let i = 0; i < 400; i++) {
       var word = randomwords();
@@ -20,21 +22,19 @@ const Main = () => {
   useEffect(() => {
     const errorP = errorRef;
     errorP.current.innerHTML = "errorCount: " + errorCount;
-    // console.log(errorP);
   }, [errorCount]);
 
-  //   console.log(words);
   var totalWords = words.join(" ");
-  //   console.log(totalWords);
   const handleTyping = (e) => {
     e.preventDefault();
 
     var value = e.target.value;
     var valueLength = value.length;
-    // console.log("value", value);
     var wordByWord = totalWords.slice(0, valueLength);
     if (value === wordByWord) {
       console.log("MATCH");
+      setSlide({ transform: `translateX(${distance}px)` });
+      setDistance(distance - 15.5);
     } else {
       console.log("ERROR, WRONG KEY: " + errorCount);
       setErrorCount(errorCount + 1);
@@ -53,14 +53,14 @@ const Main = () => {
         <div className="card2">
           {/* <p className="totalWords">{totalWords}</p> */}
           {words.map((word) => (
-            <p className="words " key={word}>
-              {word}
+            <p className="words " style={slide} key={word}>
+              <span>{word}</span>
             </p>
           ))}
         </div>
         <input className="card input" id="input" onChange={handleTyping} />
-        <p ref={errorRef}></p>
-        Keep on typing
+        <p className="errorCount" ref={errorRef}></p>
+        Keep on typing...
       </div>
       <div className="flex flex-row m-2 flex-wrap" id="buttonsContainer"></div>
     </div>
